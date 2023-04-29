b0adb9a7b6f2b531d68122c83b76890a53b85623
Liu Jiexiang <liujiexiang@Lius-MBP.lan>
2023-04-28 21:41:32 -0700

    add comments

--- test.cpp
+++ test.cpp
@@ -4,12 +4,16 @@
 
 
 int main(){
+    // create a grid of 2 channels, 3x3
     generator grid(2, 3, 3);
     grid.generate(10, 0);
     grid.printGrid();
 
+    // process grid
     utils util(grid.getGrid());
     int numChannels = grid.numChannels();
+
+    // testing sliding kernel
     for(unsigned int i = 0; i < numChannels; i++){
         std::cout << "Channel " << i << " windows" << std::endl;
         util.slidingWindow(2, 2, 1, i);
